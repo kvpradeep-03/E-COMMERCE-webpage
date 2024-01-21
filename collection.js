badb111441a346043c190302496766b5ff38c257
Kvpradeep2003 <kvpradeep60@gmail.com>
2024-01-21 22:53:49 +0530

    First commit

---
+++ collection.js
@@ -0,0 +1,18 @@
+var productContainer = document.getElementById("products")
+var search = document.getElementById("search")
+var productlist = productContainer.querySelectorAll("div")
+
+search.addEventListener("keyup",function(){
+    var enteredValue = event.target.value.toUpperCase()
+
+    for(count=0;count<productlist.length;count++){
+        var productname = productlist[count].querySelector("p").textContent
+
+        if(productname.toUpperCase().indexOf(enteredValue)<0){
+            productlist[count].style.display="none"
+        }
+        else{
+            productlist[count].style.display="block"
+        }
+    }
+})
---
+++ index.js
@@ -0,0 +1,12 @@
+var sidenav = document.querySelector(".side-navbar")
+ 
+
+function showNavbar(event){
+    sidenav.style.left = "0"
+    
+}
+
+function closeNavbar(event){
+    sidenav.style.left = "-60%"
+
+}
