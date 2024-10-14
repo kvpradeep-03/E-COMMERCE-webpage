e6cc8040820c8e8db1414779e0316cd1079f2193
Kvpradeep2003 <kvpradeep60@gmail.com>
2024-10-14 21:08:16 +0530

    code structured

---
+++ js/index.js
@@ -0,0 +1,22 @@
+var sidenav = document.querySelector(".side-navbar")
+ 
+
+function showNavbar(event){
+    sidenav.style.left = "0"
+    
+}
+
+function closeNavbar(event){
+    sidenav.style.left = "-60%"
+
+}
+
+// Select all buttons with the class 'shopnow'
+var buttons = document.querySelectorAll(".shopnow");
+
+// Loop through each button and add a click event listener
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        window.location.href = "collection.html"; // Replace with the actual page URL
+    });
+});
